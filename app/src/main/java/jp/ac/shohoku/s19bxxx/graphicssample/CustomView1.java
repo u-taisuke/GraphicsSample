43223ff3f8c626c47055f14d21ab2f8f03e0aebf
u-taisuke <utsumi@shohoku.ac.jp>
2020-10-02 11:06:19 +0900

    最初のプロジェクト作成と、CustomView1、CustomView2の作成。

---
+++ app/src/main/java/jp/ac/shohoku/s19bxxx/graphicssample/CustomView1.java
@@ -0,0 +1,21 @@
+package jp.ac.shohoku.s19bxxx.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView1 extends View {
+    public CustomView1(Context context) {
+        super(context);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView1(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
---
+++ app/src/main/java/jp/ac/shohoku/s19bxxx/graphicssample/CustomView2.java
@@ -0,0 +1,21 @@
+package jp.ac.shohoku.s19bxxx.graphicssample;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class CustomView2 extends View {
+    public CustomView2(Context context) {
+        super(context);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomView2(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
